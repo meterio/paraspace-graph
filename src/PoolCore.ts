05c17d3839dda1ea96bd5ab3ae22202e6b27790d
Alan WANG <alan1995wang@outlook.com>
2022-11-09 06:06:01 +0800

    init commit

---
+++ src/PoolCore.ts
@@ -0,0 +1,230 @@
+import {
+  Borrow,
+  FlashClaim,
+  LiquidateERC20,
+  LiquidateERC721,
+  Repay,
+  ReserveUsedAsCollateralDisabled,
+  ReserveUsedAsCollateralEnabled,
+  Supply,
+  SupplyERC721,
+  Withdraw,
+  WithdrawERC721,
+} from "../generated/PoolCore/Contract";
+import {
+  PoolCoreBorrowEntity,
+  PoolCoreFlashClaimEntity,
+  PoolCoreLiquidateERC20Entity,
+  PoolCoreLiquidateERC721Entity,
+  PoolCoreRepayEntity,
+  PoolCoreReserveUsedAsCollateralDisabledEntity,
+  PoolCoreReserveUsedAsCollateralEnabledEntity,
+  PoolCoreSupplyEntity,
+  PoolCoreSupplyERC721Entity,
+  PoolCoreWithdrawEntity,
+  PoolCoreWithdrawERC721Entity,
+} from "../generated/schema";
+import {
+  PoolCoreSupplyERC721TokenData__factory,
+} from "./factories"
+import { log } from "@graphprotocol/graph-ts";
+
+export function handleBorrow(event: Borrow): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreBorrowEntity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreBorrowEntity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.onBehalfOf = event.params.onBehalfOf
+  entity.amount = event.params.amount
+  entity.borrowRate = event.params.borrowRate
+  entity.referralCode = event.params.referralCode
+  entity.save();
+}
+export function handleFlashClaim(event: FlashClaim): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreFlashClaimEntity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreFlashClaimEntity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.target = event.params.target
+  entity.initiator = event.params.initiator
+  entity.nftAsset = event.params.nftAsset
+  entity.tokenId = event.params.tokenId
+  entity.save();
+}
+export function handleLiquidateERC20(event: LiquidateERC20): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreLiquidateERC20Entity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreLiquidateERC20Entity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.collateralAsset = event.params.collateralAsset
+  entity.liquidationAsset = event.params.liquidationAsset
+  entity.borrower = event.params.borrower
+  entity.liquidationAmount = event.params.liquidationAmount
+  entity.liquidatedCollateralAmount = event.params.liquidatedCollateralAmount
+  entity.liquidator = event.params.liquidator
+  entity.receivePToken = event.params.receivePToken
+  entity.save();
+}
+export function handleLiquidateERC721(event: LiquidateERC721): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreLiquidateERC721Entity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreLiquidateERC721Entity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.collateralAsset = event.params.collateralAsset
+  entity.liquidationAsset = event.params.liquidationAsset
+  entity.borrower = event.params.borrower
+  entity.liquidationAmount = event.params.liquidationAmount
+  entity.liquidatedCollateralTokenId = event.params.liquidatedCollateralTokenId
+  entity.liquidator = event.params.liquidator
+  entity.receiveNToken = event.params.receiveNToken
+  entity.save();
+}
+export function handleRepay(event: Repay): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreRepayEntity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreRepayEntity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.repayer = event.params.repayer
+  entity.amount = event.params.amount
+  entity.usePTokens = event.params.usePTokens
+  entity.save();
+}
+export function handleReserveUsedAsCollateralDisabled(event: ReserveUsedAsCollateralDisabled): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreReserveUsedAsCollateralDisabledEntity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreReserveUsedAsCollateralDisabledEntity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.save();
+}
+export function handleReserveUsedAsCollateralEnabled(event: ReserveUsedAsCollateralEnabled): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreReserveUsedAsCollateralEnabledEntity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreReserveUsedAsCollateralEnabledEntity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.save();
+}
+export function handleSupply(event: Supply): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreSupplyEntity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreSupplyEntity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.onBehalfOf = event.params.onBehalfOf
+  entity.amount = event.params.amount
+  entity.referralCode = event.params.referralCode
+  entity.save();
+}
+export function handleSupplyERC721(event: SupplyERC721): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreSupplyERC721Entity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreSupplyERC721Entity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.onBehalfOf = event.params.onBehalfOf
+  entity.tokenData = []
+  for(let i = 0; i < event.params.tokenData.length; i++) {
+    const e = event.params.tokenData[i];
+    entity.tokenData.push(PoolCoreSupplyERC721TokenData__factory(e, ID+i.toString()))
+  }
+  entity.referralCode = event.params.referralCode
+  entity.fromNToken = event.params.fromNToken
+  entity.save();
+}
+export function handleWithdraw(event: Withdraw): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreWithdrawEntity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreWithdrawEntity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.to = event.params.to
+  entity.amount = event.params.amount
+  entity.save();
+}
+export function handleWithdrawERC721(event: WithdrawERC721): void {
+  const ID = `${event.transaction.hash.toHex()}-${event.transactionLogIndex.toString()}`;
+
+  if (!!PoolCoreWithdrawERC721Entity.load(ID)) {
+    log.warning("Entity({}) exists!", [ID]);
+  }
+
+  let entity = new PoolCoreWithdrawERC721Entity(ID);
+  entity.block = event.block.number;
+  entity.msgSender = event.transaction.from;
+  entity.transactionHash = event.transaction.hash;
+  entity.reserve = event.params.reserve
+  entity.user = event.params.user
+  entity.to = event.params.to
+  entity.tokenIds = event.params.tokenIds
+  entity.save();
+}
---
+++ src/factories.ts
@@ -0,0 +1,153 @@
+import {
+  PoolCoreSupplyERC721TokenData,
+  PoolMarketplaceAcceptBidWithCreditOrderInfoOffer,
+  PoolMarketplaceAcceptBidWithCreditOrderInfoConsideration,
+  PoolMarketplaceAcceptBidWithCreditOrderInfo,
+  PoolMarketplaceAcceptBidWithCreditCredit,
+  PoolMarketplaceBuyWithCreditOrderInfoOffer,
+  PoolMarketplaceBuyWithCreditOrderInfoConsideration,
+  PoolMarketplaceBuyWithCreditOrderInfo,
+  PoolMarketplaceBuyWithCreditCredit,
+} from "../generated/schema"
+
+import {
+  SupplyERC721TokenDataStruct,
+} from "../generated/PoolCore/Contract"
+import {
+  AcceptBidWithCreditOrderInfoOfferStruct,
+  AcceptBidWithCreditOrderInfoConsiderationStruct,
+  AcceptBidWithCreditOrderInfoStruct,
+  AcceptBidWithCreditCreditStruct,
+  BuyWithCreditOrderInfoOfferStruct,
+  BuyWithCreditOrderInfoConsiderationStruct,
+  BuyWithCreditOrderInfoStruct,
+  BuyWithCreditCreditStruct,
+} from "../generated/PoolMarketplace/Contract"
+
+// PoolCore
+
+type FPoolCoreSupplyERC721TokenData = (params: SupplyERC721TokenDataStruct, id: string) => string
+export const PoolCoreSupplyERC721TokenData__factory: FPoolCoreSupplyERC721TokenData = (params, id) => {
+  const entity = new PoolCoreSupplyERC721TokenData(id);
+  entity.tokenId = params.tokenId;
+  entity.useAsCollateral = params.useAsCollateral;
+  entity.save();
+  return id;
+}
+// PoolMarketplace
+
+type FPoolMarketplaceAcceptBidWithCreditOrderInfoOffer = (params: AcceptBidWithCreditOrderInfoOfferStruct, id: string) => string
+export const PoolMarketplaceAcceptBidWithCreditOrderInfoOffer__factory: FPoolMarketplaceAcceptBidWithCreditOrderInfoOffer = (params, id) => {
+  const entity = new PoolMarketplaceAcceptBidWithCreditOrderInfoOffer(id);
+  entity.itemType = params.itemType;
+  entity.token = params.token;
+  entity.identifierOrCriteria = params.identifierOrCriteria;
+  entity.startAmount = params.startAmount;
+  entity.endAmount = params.endAmount;
+  entity.save();
+  return id;
+}
+
+type FPoolMarketplaceAcceptBidWithCreditOrderInfoConsideration = (params: AcceptBidWithCreditOrderInfoConsiderationStruct, id: string) => string
+export const PoolMarketplaceAcceptBidWithCreditOrderInfoConsideration__factory: FPoolMarketplaceAcceptBidWithCreditOrderInfoConsideration = (params, id) => {
+  const entity = new PoolMarketplaceAcceptBidWithCreditOrderInfoConsideration(id);
+  entity.itemType = params.itemType;
+  entity.token = params.token;
+  entity.identifierOrCriteria = params.identifierOrCriteria;
+  entity.startAmount = params.startAmount;
+  entity.endAmount = params.endAmount;
+  entity.recipient = params.recipient;
+  entity.save();
+  return id;
+}
+
+type FPoolMarketplaceAcceptBidWithCreditOrderInfo = (params: AcceptBidWithCreditOrderInfoStruct, id: string) => string
+export const PoolMarketplaceAcceptBidWithCreditOrderInfo__factory: FPoolMarketplaceAcceptBidWithCreditOrderInfo = (params, id) => {
+  const entity = new PoolMarketplaceAcceptBidWithCreditOrderInfo(id);
+  entity.maker = params.maker;
+  entity.taker = params.taker;
+  entity.tid = params.id;
+  entity.offer = []
+  for(let i = 0; i < params.offer.length; i++) {
+    const e = params.offer[i];
+    entity.offer.push(PoolMarketplaceAcceptBidWithCreditOrderInfoOffer__factory(e, id+i.toString()))
+  }
+  entity.consideration = []
+  for(let i = 0; i < params.consideration.length; i++) {
+    const e = params.consideration[i];
+    entity.consideration.push(PoolMarketplaceAcceptBidWithCreditOrderInfoConsideration__factory(e, id+i.toString()))
+  }
+  entity.save();
+  return id;
+}
+
+type FPoolMarketplaceAcceptBidWithCreditCredit = (params: AcceptBidWithCreditCreditStruct, id: string) => string
+export const PoolMarketplaceAcceptBidWithCreditCredit__factory: FPoolMarketplaceAcceptBidWithCreditCredit = (params, id) => {
+  const entity = new PoolMarketplaceAcceptBidWithCreditCredit(id);
+  entity.token = params.token;
+  entity.amount = params.amount;
+  entity.orderId = params.orderId;
+  entity.v = params.v;
+  entity.r = params.r;
+  entity.s = params.s;
+  entity.save();
+  return id;
+}
+
+type FPoolMarketplaceBuyWithCreditOrderInfoOffer = (params: BuyWithCreditOrderInfoOfferStruct, id: string) => string
+export const PoolMarketplaceBuyWithCreditOrderInfoOffer__factory: FPoolMarketplaceBuyWithCreditOrderInfoOffer = (params, id) => {
+  const entity = new PoolMarketplaceBuyWithCreditOrderInfoOffer(id);
+  entity.itemType = params.itemType;
+  entity.token = params.token;
+  entity.identifierOrCriteria = params.identifierOrCriteria;
+  entity.startAmount = params.startAmount;
+  entity.endAmount = params.endAmount;
+  entity.save();
+  return id;
+}
+
+type FPoolMarketplaceBuyWithCreditOrderInfoConsideration = (params: BuyWithCreditOrderInfoConsiderationStruct, id: string) => string
+export const PoolMarketplaceBuyWithCreditOrderInfoConsideration__factory: FPoolMarketplaceBuyWithCreditOrderInfoConsideration = (params, id) => {
+  const entity = new PoolMarketplaceBuyWithCreditOrderInfoConsideration(id);
+  entity.itemType = params.itemType;
+  entity.token = params.token;
+  entity.identifierOrCriteria = params.identifierOrCriteria;
+  entity.startAmount = params.startAmount;
+  entity.endAmount = params.endAmount;
+  entity.recipient = params.recipient;
+  entity.save();
+  return id;
+}
+
+type FPoolMarketplaceBuyWithCreditOrderInfo = (params: BuyWithCreditOrderInfoStruct, id: string) => string
+export const PoolMarketplaceBuyWithCreditOrderInfo__factory: FPoolMarketplaceBuyWithCreditOrderInfo = (params, id) => {
+  const entity = new PoolMarketplaceBuyWithCreditOrderInfo(id);
+  entity.maker = params.maker;
+  entity.taker = params.taker;
+  entity.tid = params.id;
+  entity.offer = []
+  for(let i = 0; i < params.offer.length; i++) {
+    const e = params.offer[i];
+    entity.offer.push(PoolMarketplaceBuyWithCreditOrderInfoOffer__factory(e, id+i.toString()))
+  }
+  entity.consideration = []
+  for(let i = 0; i < params.consideration.length; i++) {
+    const e = params.consideration[i];
+    entity.consideration.push(PoolMarketplaceBuyWithCreditOrderInfoConsideration__factory(e, id+i.toString()))
+  }
+  entity.save();
+  return id;
+}
+
+type FPoolMarketplaceBuyWithCreditCredit = (params: BuyWithCreditCreditStruct, id: string) => string
+export const PoolMarketplaceBuyWithCreditCredit__factory: FPoolMarketplaceBuyWithCreditCredit = (params, id) => {
+  const entity = new PoolMarketplaceBuyWithCreditCredit(id);
+  entity.token = params.token;
+  entity.amount = params.amount;
+  entity.orderId = params.orderId;
+  entity.v = params.v;
+  entity.r = params.r;
+  entity.s = params.s;
+  entity.save();
+  return id;
+}
